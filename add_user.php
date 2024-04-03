b6111f88d1a2d45f6d071e6983f2b92c45121256
Pcommit final <poaris@zend-poaris.fjeclot.net>
2024-04-03 17:00:30 +0200

    commit final

--- add_user.php
+++ add_user.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     try {
         $ldap->add($dn, $datos);
         echo "<p>Usuario añadido con éxito.</p>";
-        echo "<a href='http://zend-poaris.fjeclot.net/projecte//'>ir al menu principal</a>";
+        echo "<a href='https://zends-poaris/proyectom08uf23/menu.php'>ir al menu principal</a>";
     } catch (\Laminas\Ldap\Exception\LdapException $e) {
         echo "<p>Error al añadir el usuario: " . $e->getMessage() . "</p>";
     }
